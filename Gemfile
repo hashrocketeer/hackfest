64bf3f25e14554b91943e7bb2fcd636ed6b7d0e0
Dave Lyon and Aaron Kalin <dev+davelyon+martinisoft@hashrocket.com>
2011-07-23 11:21:42 -0500

    Rails template run

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails',     :git => 'git://github.com/rails/rails.git', branch: '3-1-stable'
+
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackfest::Application.config.secret_token = 'be841a855f1aba41cf074245f902a0f217e85481bab5226830bbeabf9cb030c9714622e5528ed02849805a250cb1cc98fd7b0b989594d3c0c0021b9afc8a1803'
