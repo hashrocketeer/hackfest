3d4bb35603ad9da0c551bdd92d179ebf5f68a59b
Dave Lyon and Aaron Kalin <dev+davelyon+martinisoft@hashrocket.com>
2011-07-23 11:48:43 -0500

    Set rspec and haml generator defaults

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,11 @@ module Hackfest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Generator Settings
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, fixture: true, views: false
+    end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
